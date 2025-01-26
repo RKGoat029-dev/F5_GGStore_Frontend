f49a1baf1a7bb48a9f9b22f03edbda188c86c0ba
RKGoat029-dev <rkgoat029.dev@gmail.com>
2025-01-26 12:20:14 +0100

    minor fixes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import Footer from "../../components/molecules/Footer/Footer.jsx";
 import Header from "../../components/molecules/Header/Header.jsx";
+
 import "./home.css";
 
 const Home = () => {
