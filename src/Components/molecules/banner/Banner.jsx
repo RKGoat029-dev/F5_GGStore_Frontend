91684386f107776dc44a9ceb5c61bbf7436bf291
nawud <duwan17@gmail.com>
2025-01-26 21:06:29 +0100

    estilos

---
+++ src/Components/molecules/banner/Banner.jsx
@@ -0,0 +1,11 @@
+import banner from "../../../assets/icon/banner.png"
+
+function Banner() {
+  return (
+    <div>
+      <img className="banner-logo w-500 h-150" src={banner} alt="" />
+    </div>
+  )
+}
+
+export default Banner
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,11 +2,13 @@ import Header from "../../components/molecules/Header/Header.jsx";
 import ProfileCard from "../../components/organisms/cards/ProfileCard.jsx";
 import Footer from "../../components/molecules/Footer/Footer.jsx";
 import "./home.css";
+import Banner from "../../components/molecules/banner/Banner.jsx";
 
 const Home = () => {
   return (
     <>
         <Header />
+        <Banner />
         <ProfileCard />
         <Footer />
     </>
